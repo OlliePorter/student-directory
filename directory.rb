3ca70038b8a57aefe0a759dc69f0ffa351435363
Ollie Porter <ol.porter@me.com>
2019-05-12 17:51:06 +0100

    Completed student-directory exercises up to exercise 11

--- directory.rb
+++ directory.rb
@@ -4,29 +4,41 @@ def input_students
   students = []
   name = gets.chomp
   while !name.empty? do
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+    puts "Please enter the date of birth for that student"
+    date_of_birth = gets.chomp
+    puts "Please enter their cohort"
+    cohort = gets.chomp
+    if cohort.empty? == true
+      cohort = :not_given
+    end
+    students << {name: name, date_of_birth: date_of_birth, cohort: cohort}
+    if students.length < 2
+      puts "Now we have #{students.count} student"
+      puts "Please provide another student name"
+    else
+      puts "Now we have #{students.count} students"
+      puts "Please provide another student name"
+    end
     name = gets.chomp
   end
   students
 end
 
-def print_header
-  puts "The students of Villains Academy"
-  puts "------------"
-end
-
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  puts "Please enter the cohort you'd like to filter by e.g. 'May 2019'"
+  cohort_filter = gets.chomp
+  puts "The students of Villains Academy".center(500)
+  puts "------------".center(500)
+  filtered_students = students.select { |item| item[:cohort] == cohort_filter}
+  filtered_students.each_with_index do |student, index|
+    puts "Name: #{student[:name]} - DOB: #{student[:date_of_birth]} - Cohort: #{student[:cohort]}".center(500)
   end
 end
 
 def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+  puts "Overall, we have #{names.count} great students".center(500)
 end
 
 students = input_students
-print_header
 print(students)
 print_footer(students)
